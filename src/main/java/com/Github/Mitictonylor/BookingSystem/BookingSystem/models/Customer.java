797bc1b717e9057ea0916fd8ec7ea75c6cec4aaf
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-11 11:18:07 +0100

    create variables

---
+++ src/main/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/models/Customer.java
@@ -0,0 +1,23 @@
+package com.Github.Mitictonylor.BookingSystem.BookingSystem.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String town;
+
+    @Column
+    private int age;
+
+
+}
