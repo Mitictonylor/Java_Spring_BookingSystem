354a9bfbaa67145bf00dd205d70c3881ba3710a4
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-11 11:19:03 +0100

    add getters and setters

--- src/main/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/models/Customer.java
+++ src/main/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/models/Customer.java
@@ -27,4 +27,36 @@ public class Customer {
 
     public Customer() {
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
