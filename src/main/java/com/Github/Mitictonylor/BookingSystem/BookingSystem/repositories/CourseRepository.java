873d03630fe9d742a5c1f8a66cdf931c41d765b1
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-11 13:40:15 +0100

    test findByStarRating test pass

--- src/main/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/repositories/CourseRepository.java
+++ src/main/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/repositories/CourseRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    List<Course> FindByRating(int rating);
+    List<Course> findByStarRating(int rating);
 
 }
--- src/test/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/BookingSystemApplicationTests.java
+++ src/test/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/BookingSystemApplicationTests.java
@@ -1,13 +1,29 @@
 package com.Github.Mitictonylor.BookingSystem.BookingSystem;
 
+import com.Github.Mitictonylor.BookingSystem.BookingSystem.models.Course;
+import com.Github.Mitictonylor.BookingSystem.BookingSystem.repositories.CourseRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class BookingSystemApplicationTests {
+
+	@Autowired
+	CourseRepository courseRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void findCoursesByRating() {
+		List<Course> foundCourse = courseRepository.findByStarRating(4);
+		assertEquals(3, foundCourse.size() );
+	}
 }
