f91054cf7a642b50d6b46b20bb35a659d177d3b2
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-11 15:49:01 +0100

    add get mapping + getmapping{id_}

---
+++ src/main/java/com/Github/Mitictonylor/BookingSystem/BookingSystem/controller/CustomerController.java
@@ -0,0 +1,46 @@
+package com.Github.Mitictonylor.BookingSystem.BookingSystem.controller;
+
+import com.Github.Mitictonylor.BookingSystem.BookingSystem.models.Customer;
+import com.Github.Mitictonylor.BookingSystem.BookingSystem.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers(
+            @RequestParam(name ="courseName", required = false)String courseName,
+            @RequestParam(name = "town",required = false)String town,
+            @RequestParam(name = "ageOver", required = false)Integer age
+    ){
+        if(courseName != null & town != null & age != null){
+            return new ResponseEntity<>(customerRepository.findByTownAndBookingsCourseNameAndAgeGreaterThan(town,courseName,age), HttpStatus.OK);
+        }
+        if(courseName != null & town != null){
+            return new ResponseEntity<>(customerRepository.findByTownAndBookingsCourseName(town,courseName), HttpStatus.OK);
+        }
+        if (courseName != null){
+            return new ResponseEntity<>(customerRepository.findByBookingsCourseName(courseName), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "customers/{id}")
+    public ResponseEntity getCustomerById(@PathVariable Long id){
+        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value)
+
+
+}
+
+
